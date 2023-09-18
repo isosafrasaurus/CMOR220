4d316f1361a803db10206f32672e70396b8f8bee
isosafrasaurus <143103585+isosafrasaurus@users.noreply.github.com>
2023-09-18 05:00:02 -0500

    cooldrive bisect broken

--- Root/cooldrive.m
+++ Root/cooldrive.m
@@ -7,7 +7,10 @@
 function cooldrive()
     % set a, t and a range of L and b and find x
     % and plot x^2 against L
+    a = 0.1; b = 0.3; t = 0.01;
     L = 1 : 0.1 : 4;
+
+    x = zeros(length(L));
     for cnt = 1:length(L)
         x(cnt) = debis(a,b,t,L(cnt));
     end
@@ -17,14 +20,24 @@ end
 
 function[x,iter]=denewt(x,tol,L)
     %Newtons method, calls coolfun and coolfundx
-    while (abs(coolfun(x)) > tol)
-        x = x - coolfun(x)/coolfundx(x);
+    while (abs(coolfun(x,L)) > tol)
+        x = x - coolfun(x,L)/coolfundx(x,L);
     end
 end
 
 function[x,iter]=debis(a,b,t,L) % for a given a, b, t, and L find x
     % code the bisection method here
-    
+    x = a;
+    while (abs(coolfun(x,L)) > t)
+        x = (a + b) / 2;
+        if (coolfun(a,L) * coolfun(b,L) > 0)
+            disp("no solution in a,b");
+        elseif coolfun(a,L) * coolfun(x,L) < 0
+            b = x;
+        else
+            a = x;
+        end
+    end
 end
 
 function val = coolfun(x,L) %for a given x and L evaluate "cool"
